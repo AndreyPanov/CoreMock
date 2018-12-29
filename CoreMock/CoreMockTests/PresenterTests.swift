32600cc13903501e8e0a5f6fa5501e0f25e2a1b7
Andrey Panov <andrey.panov@blacklane.com>
2018-12-29 14:18:35 +0100

    Add mocks and tests

--- CoreMock/CoreMockTests/PresenterTests.swift
+++ CoreMock/CoreMockTests/PresenterTests.swift
@@ -3,13 +3,41 @@ import XCTest
 
 class PresenterTests: BaseTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  private var presenter: Presenter!
+  private var view: ViewMock!
+  private var repository: RepositoryMock!
+  
+  override func setUp() {
+    super.setUp()
+    
+    view = ViewMock(with: self)
+    repository = RepositoryMock(with: self)
+    presenter = Presenter(view: view, repository: repository)
+  }
+  
+  override func tearDown() {
+    view = nil
+    repository = nil
+    presenter = nil
+    
+    super.tearDown()
+  }
+  
+  func testOnViewDidLoad() {
+    presenter.onViewDidLoad()
+    
+    verify(view).set(title: "Text")
+    AssertMockCallHistoryTrue()
+  }
+  
+  func testOnUpdateTapped() {
+    presenter.onUpdateTapped()
+    
+    verify(repository).getItems(onSuccess: {_ in })
+    verify(view).set(boardVisible: true)
+    verify(view).set(title: "Updated")
+    AssertMockCallHistoryTrue()
+  }
 }
 
 
@@ -23,5 +51,38 @@ class ViewMock: View, Mock {
   
   func instanceType() -> ViewMock {
     return self
+  }
+  
+  func set(title: String) {
+    callHandler
+      .accept(withFunction: #function, inFile: #file, atLine: #line)
+      .join(arg: .string(title))
+      .check(withFunction: #function, inFile: #file, atLine: #line)
+  }
+  
+  func set(boardVisible: Bool) {
+    callHandler
+      .accept(withFunction: #function, inFile: #file, atLine: #line)
+      .join(arg: .bool(boardVisible))
+      .check(withFunction: #function, inFile: #file, atLine: #line)
+  }
+}
+
+class RepositoryMock: Repository, Mock {
+  let callHandler: CallHandler
+  
+  init(with testCase: BaseTestCase) {
+    callHandler = CallHandler(withTestCase: testCase)
+  }
+  
+  func instanceType() -> RepositoryMock {
+    return self
+  }
+  
+  func getItems(onSuccess: @escaping ([String]) -> Void) {
+    callHandler
+      .accept(withFunction: #function, inFile: #file, atLine: #line)
+      .check(withFunction: #function, inFile: #file, atLine: #line)
+    onSuccess(["Cat", "Dog"])
   }
 }
